66795f67dee1e3a210d625487a234e89bcbb0864
Gulchatai <sgalinaval@gmail.com>
2023-04-28 05:29:55 +0300

    1 commit

---
+++ src/main/java/proskycousrs2workingwithexceptions/WorkingWithExceptionsApplication.java
@@ -0,0 +1,13 @@
+package proskycousrs2workingwithexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingWithExceptionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkingWithExceptionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/proskycousrs2workingwithexceptions/WorkingWithExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package proskycousrs2workingwithexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingWithExceptionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
